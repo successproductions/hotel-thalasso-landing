ce417e572b4bd32e189ba769e730fc7010d519d7
mohammed daali <w.master@successproductions.ma>
2025-06-16 18:17:04 +0100

    fix: refactor LocaleLayout for improved readability and structure; enhance schema definition for better SEO

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,31 +1,49 @@
-
 import '../globals.css';
 import { NextIntlClientProvider } from 'next-intl';
 import { ThemeProvider }          from 'next-themes';
 import type { ReactNode }         from 'react';
 
 export async function generateStaticParams() {
-  
-  return [
-    { locale: 'fr' },
-    { locale: 'en' }
-  ];
+  return [{ locale: 'fr' }, { locale: 'en' }];
 }
 
-export default async function LocaleLayout(
- { children, params }: {
+export default async function LocaleLayout({
+  children,
+  params,
+}: {
   children: ReactNode;
   params: Promise<{ locale: 'fr' | 'en' }>;
-}
-) {
-
-
+}) {
   const { locale } = await params;
-const messages = (await import(`../../messages/${locale}.json`)).default;
+  const messages   = (await import(`../../messages/${locale}.json`)).default;
 
+  
+  const schema = {
+    "@context": "https://schema.org",
+    "@type": "HealthAndBeautyBusiness",
+    "name": "DC Thermes – Évasion Holistique",
+    "url":
+      locale === "en"
+        ? "https://offer.dakhlaclub.com/en"
+        : "https://offer.dakhlaclub.com/fr",
+    "openingHours": "Mo-Su 09:00-19:00",
+    "address": {
+      "@type": "PostalAddress",
+      "addressLocality": "Dakhla",
+      "addressCountry": "MA",
+    },
+  };
 
   return (
     <html lang={locale} suppressHydrationWarning>
+      <head>
+        <script
+          type="application/ld+json"
+          suppressHydrationWarning
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
+        />
+      </head>
+
       <body>
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
           <NextIntlClientProvider locale={locale} messages={messages}>
