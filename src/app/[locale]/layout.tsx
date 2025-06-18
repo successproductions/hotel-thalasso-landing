e5f18c604b7e9e990df9effb0ca0408d9969c06a
mohammed daali <w.master@successproductions.ma>
2025-06-18 12:02:57 +0100

    feat: wrap children in ClientMountGuard and Suspense for improved loading handling in RootLayout

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -3,9 +3,7 @@ import { NextIntlClientProvider } from 'next-intl';
 import { ThemeProvider } from 'next-themes';
 import type { ReactNode } from 'react';
 import type { Metadata } from 'next';
-import { Suspense } from 'react';
-import ClientMountGuard from '../ClientMountGuard';
-import Loading from '../loading';
+
 
 export async function generateStaticParams() {
   return [{ locale: 'fr' }, { locale: 'en' }];
@@ -194,11 +192,8 @@ export default async function LocaleLayout({
 
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
           <NextIntlClientProvider locale={locale} messages={messages}>
-          <ClientMountGuard>
-          <Suspense fallback={<Loading />}>
+
             {children}
-            </Suspense>
-            </ClientMountGuard>
           </NextIntlClientProvider>
         </ThemeProvider>
       </body>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,15 @@
 import './globals.css';
 import type { ReactNode } from 'react';
+import ClientMountGuard from './ClientMountGuard'; 
+import { Suspense } from 'react';
+import Loading from './loading';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-  return children; 
+  return (
+    <ClientMountGuard>
+      <Suspense fallback={<Loading />}>
+        {children}
+      </Suspense>
+    </ClientMountGuard>
+  );
 }
