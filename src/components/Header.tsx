b7893f950244b362971b2bc9e5ae96d518838c68
mohammed daali <w.master@successproductions.ma>
2025-07-04 16:58:12 +0100

    fix: enhance button styles in Header component for improved UI consistency

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -128,9 +128,9 @@ export default function Header() {
           <Button
             size="sm"
             className={clsx(
-              " px-5 transition-transform",
+              " px-5 transition-transform rounded-full py-5",
               active
-                ? "bg-[#139584] text-white hover:bg-green-700 hover:scale-105 hover:shadow-xl"
+                ? "bg-[#139584] text-white hover:bg-[#d6bb8e] hover:scale-105 hover:shadow-xl"
                 : "bg-white text-green-900 hover:bg-gray-100 dark:bg-gray-800 dark:text-white"
             )}
             onClick={() => {
