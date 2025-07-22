8138652b20128c36993cf94bfe838e5b5186e0e6
mohammed daali <w.master@successproductions.ma>
2025-07-22 09:40:08 +0100

    refactor: change localePrefix in routing configuration from 'as-needed' to 'always' for consistent URL structure

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -3,7 +3,7 @@ import { defineRouting } from 'next-intl/routing';
 export const routing = defineRouting({
   locales: ['fr', 'en'],
   defaultLocale: 'fr',
-  localePrefix: 'as-needed',
+  localePrefix: 'always', 
   pathnames: {
     '/': '/',
     '/evasion-holistique-3-jours': {
