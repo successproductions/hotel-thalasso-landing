827dd1440852d7fef0cd990237041c5b2d6f57f8
mohammed daali <w.master@successproductions.ma>
2025-10-17 11:39:55 +0100

    add tags for ads and chant activites

--- src/app/[locale]/halloween/page.tsx
+++ src/app/[locale]/halloween/page.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import HalloweenHeader from '@/components/halloween/HalloweenHeader';
 import HalloweenFooter from '@/components/halloween/HalloweenFooter';
 import HalloweenHero from '@/components/halloween/HalloweenHero';
@@ -103,6 +104,29 @@ export default async function HalloweenPage({
 
   return (
     <>
+      {/* Google Tag Manager - Halloween Page */}
+      <Script
+        id="gtm-halloween"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-PHTBVLGW');`,
+        }}
+      />
+
+      {/* Google Tag Manager (noscript) - Halloween Page */}
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-PHTBVLGW"
+          height="0"
+          width="0"
+          style={{ display: 'none', visibility: 'hidden' }}
+        ></iframe>
+      </noscript>
+
       <HalloweenHeader locale={locale} />
       <main className="bg-white">
         <div id="hero">
